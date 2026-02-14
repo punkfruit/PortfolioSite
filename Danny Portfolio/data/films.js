c9d66e49b565c39d1bd259d90c7cf67185318eff
CMVanwagoner <cmvanwagoner@gmail.com>
2026-02-14 12:56:47 -0700

    first upload

---
+++ Danny Portfolio/data/films.js
@@ -0,0 +1,20 @@
+export const films = [
+  {
+    "title": "Documentary (Neumi Doc)",
+    "youtubeId": "JG2eNtwXPvE",
+    "date": "April 2025",
+    "description": "Documentary made for Film Making I"
+  },
+  {
+    "title": "Short with Friends (FirstAssignment)",
+    "youtubeId": "kaC1fsvojUY",
+    "date": "January 2026",
+    "description": "Short I directed with my colleagues"
+  },
+  {
+    "title": "Egg Montage",
+    "youtubeId": "o5yDhZBedUo",
+    "date": "TBD",
+    "description": "Brief description"
+  }
+]
