6d84bf1f4942a2184c6672522d7ab379e794ce52
punkfruit <thecybergrape@gmail.com>
2026-02-20 12:00:39 -0700

    Extra credit upload

--- data/films.js
+++ data/films.js
@@ -1,20 +1,26 @@
 export const films = [
-  {
-    "title": "Documentary (Neumi Doc)",
-    "youtubeId": "JG2eNtwXPvE",
-    "date": "April 2025",
-    "description": "Documentary made for Film Making I"
-  },
-  {
-    "title": "Short with Friends (FirstAssignment)",
-    "youtubeId": "kaC1fsvojUY",
-    "date": "January 2026",
-    "description": "Short I directed with my colleagues"
-  },
-  {
-    "title": "Egg Montage",
-    "youtubeId": "o5yDhZBedUo",
-    "date": "TBD",
-    "description": "Brief description"
-  }
-]
+   {
+      "title":"Extra Credit",
+      "youtubeId":"corOI9HU39Y",
+      "date":"February 2026",
+      "description":"Group Chemistry assignment i directed"
+   },
+   {
+      "title":"Documentary (Neumi Doc)",
+      "youtubeId":"JG2eNtwXPvE",
+      "date":"April 2025",
+      "description":"Documentary made for Film Making I"
+   },
+   {
+      "title":"Short with Friends (FirstAssignment)",
+      "youtubeId":"kaC1fsvojUY",
+      "date":"January 2026",
+      "description":"Short I directed with my colleagues"
+   },
+   {
+      "title":"Egg Montage",
+      "youtubeId":"o5yDhZBedUo",
+      "date":"TBD",
+      "description":"Brief description"
+   }
+  ] 
