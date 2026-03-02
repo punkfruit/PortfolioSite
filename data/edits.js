b0ac1078c2645c0a97393a75755247ab0424ccb3
punkfruit <thecybergrape@gmail.com>
2026-03-01 23:36:11 -0700

    editing updates

--- data/edits.js
+++ data/edits.js
@@ -3,12 +3,12 @@ export const edits = [
     "title": "Jacuzzi",
     "youtubeId": "ACLTqlzaYpM?si",
     "date": "April 2025",
-    "description": "Short film 'The Spa' that I edited"
+    "description": "Short film 'The Spa' that I edited in Premiere"
   },
   {
     "title": "Fisker",
     "youtubeId": "k2ORcUC42KQ?si",
     "date": "January 2026",
-    "description": "Commercial for the Fisker Karma that I put together"
+    "description": "Commercial for the Fisker Karma that I put together in AVID"
   }
 ]
