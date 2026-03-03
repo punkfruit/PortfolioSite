3e21e58822570a9e9d84e4ae628dbfd692a45a81
punkfruit <thecybergrape@gmail.com>
2026-03-03 01:31:07 -0700

    added more 3d models

--- data/3Dmodels.js
+++ data/3Dmodels.js
@@ -6,6 +6,13 @@ export const models = [
     date: "December 2024",
     description: "Scene of a game room I made for my class final!"
   },
+  {
+    title: "Strongman",
+    type: "sketchfab",
+    id: "5b3d5584153641fa93aa7ba515e57655",
+    date: "November 2025",
+    description: "Animated circus strongman i made for a gamejam"
+  },
    {
     title: "Lego Guy",
     type: "youtube",
@@ -33,5 +40,19 @@ export const models = [
     id: "FjEgRUiutM8",
     date: "December 2023",
     description: "Sandal I made in Blender for my final"
+  },
+  {
+    title: "SNES",
+    type: "youtube",
+    id: "hlDHOmLGGvQ",
+    date: "October 2023",
+    description: "Super Nintendo modeled in Blender"
+  },
+  {
+    title: "Penguin Scene",
+    type: "youtube",
+    id: "kqEl7UT9PcQ",
+    date: "October 2023",
+    description: "Some duplo penguins  modeled in Blender"
   }
 ];
