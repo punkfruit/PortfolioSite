090b952397acb56de0911e8e31c8bc21a7e67fa9
punkfruit <thecybergrape@gmail.com>
2026-09-24 01:03:14 -0600

    added more writing
    clocking out script, 61.6 improvements

--- data/writing.js
+++ data/writing.js
@@ -2,15 +2,22 @@ export const writing = [
   {
     title: "The Time Traveler's Journal",
     type: "Screenplay",
-    logline: "A treasure hunter uses a mysterious object to travel through time, setting out to steal the Amber Room before it disappears from history.",
+    logline: "A desperate treasure hunter uses time travel to heist the legendary Amber Room before its destruction, only to discover his unexpected companion is history’s true lost relic.. and time itself wants her back",
     pdf: "pdfs/time-travelers-journal.pdf",
     previewPage: 2
   },
   {
   title: "61.6",
   type: "Short Screenplay",
-  logline: "An urban explorer livestreams from an abandoned 1990s radio station, unaware that her skyrocketing viewer count comes at a price",
+  logline: "A clout-chasing urban explorer live-streams her midnight trek into a famously abandoned radio station, unknowingly tuning into a forbidden frequency that unleashes a sinister force upon her stream.",
   pdf: "pdfs/voiceOfTheDamned01.pdf",
+  previewPage: 2
+},
+  {
+  title: "Clocking Out",
+  type: "Short Screenplay",
+  logline: "When a exhausted factory worker accidentally turns coat and becomes a corporate mouthpiece to save his own skin, he must choose between the safety of his promotion and unionizing his friends to expose a deadly company secret",
+  pdf: "pdfs/StrikeStoryFinal.pdf",
   previewPage: 2
 }
 ];
